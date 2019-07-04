4982d7d6a612b35ea85cc02395aaa715bbc1a6c6
ankitaredhat <er.ankitadewel@gmail.com>
2019-07-04 23:26:04 +0530

    Puppet: Facter resource

--- site.pp
+++ site.pp
@@ -1,29 +1,7 @@
-$web_path = '/etc/httpd/conf.d/web.conf'
-$dir = '/var/tmp/data'
+###### Exanple of FACTER Resource in Puppet #######
 
-package { 'httpd':
-
-  ensure => 'latest',
-  notify => Service['httpd']
-}
-
-file { "$web_path":
-   ensure => file,
-   owner => 'apache',
-   group => 'apache',
-   mode => '0644',
-   source => 'puppet:///modules/apache/web.conf',
-   notify => Service['httpd'],
+file { 'Use case of Facter':
+   path => '/etc/motd',
+   content => "This is your server $::osfamily family \nIP Address is $::ipaddress \nMAC address is $::macaddress \n"
 
 }
-
-service { 'httpd':
- ensure => 'running',
-}
-
-file { "$dir":
-  ensure => 'file',
-
-} 
-
-  
