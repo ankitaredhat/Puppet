3d1bbf642c75367964aa41315a321becfee73ac6
root <root@ip-172-31-15-179.us-east-2.compute.internal>
2019-07-21 07:13:39 +0000

    Example of subscribe & notify in puppet

---
+++ subscribe_notify.pp
@@ -0,0 +1,36 @@
+############### NOTIFY & SUBSCRIBE - used fir relationship and ordering ##########
+
+# Follow these stpes before :
+# [root@server ~]# cd /etc/puppet/modules/
+#[root@server modules]# mkdir -p apache/files
+
+
+
+class web {
+    package { 'httpd':
+      ensure => present,
+    }
+    file { "/var/www/website":  # Creating Document Root
+      ensure => "directory",
+      owner  => "apache",
+      group  => "apache",
+      mode   => 750,
+    }
+ file { '/var/www/website/index.html': # Creating Index file
+     ensure  => file,
+     content => "HTML Is Managed By Puppet",
+     mode    => '0644',
+   } 
+    file { '/etc/httpd/conf.d/web.conf': # Path to the file on client machine 
+      ensure => file,
+      mode   => '0600',
+      source => 'puppet:///modules/apache/web.conf', # Path to the custom file on puppet server
+      notify => 'Service[httpd]',
+    } 
+    service { 'httpd':
+      ensure => runnning,
+      restart=>'/usr/sbin/httpd',
+      enable => true,
+    }
+}
+
